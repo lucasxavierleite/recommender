2ae9d4c4198da1479c1e718f15f225b46da707ad
leonardovin <mcleozin4u@gmail.com>
2019-06-21 23:48:17 -0300

    Add files via upload

---
+++ recommender-master/filme.c
@@ -0,0 +1,160 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include "filme.h"
+#include "dicionario.h"
+#include "aux.h"
+#include "interface.h"
+
+struct filme {
+	char *nome;
+	unsigned n_generos;
+	char **generos;
+	char *sinopse;
+	unsigned ano;
+};
+
+FILME *filme_criar(char *nome, int ano, char *generos, char *sinopse) {
+	FILME *filme = (FILME *) malloc(sizeof(FILME));
+
+	filme->nome = (char *) malloc(sizeof(char) * strlen(nome) + 1);
+	strcpy(filme->nome, nome);
+	filme->ano = ano;
+	filme->generos = tokenize(generos, &filme->n_generos, ",\n");
+	filme->sinopse = (char *) malloc(sizeof(char) * strlen(sinopse) + 1);
+	strcpy(filme->sinopse, sinopse);
+
+	return filme;
+}
+
+FILME *filme_ler_arquivo(FILE *arquivo, DICIONARIO *dicionario_sinopse, DICIONARIO *stopwords) {
+	char lixo[50];
+	char nome[100];
+	int ano;
+	char generos[200];
+	char sinopse[5000];
+
+	fscanf(arquivo, "%s %[^\n]", lixo, nome);
+	fscanf(arquivo, "%s %d",  lixo, &ano);
+	fscanf(arquivo, "%s %[^\n]", lixo, generos);
+	fscanf(arquivo, "%s %[^\n] ", lixo, sinopse);
+
+	int nv = 0;
+	char sinopse_cp[strlen(sinopse)];
+	strcpy(sinopse_cp, sinopse);
+	
+	char **v = tokenize_stopwords(sinopse_cp, &nv, NAO_ALFA, stopwords);
+
+	for(int i = 0; i < nv; i++) {
+		dicionario_inserir(dicionario_sinopse, v[i]);
+	}
+
+	return filme_criar(nome, ano, generos, sinopse);
+}
+
+double filme_calcula_peso_nome(DICIONARIO* stopwords, FILME *f1, FILME *f2) {	
+	int nv1 = 0, nv2 = 0;
+
+	char f1_cp[strlen(f1->nome)];
+	strcpy(f1_cp, f1->nome);
+	char **v1 = tokenize_stopwords(f1_cp, &nv1, NAO_ALFA, stopwords);
+
+	char f2_cp[strlen(f2->nome)];
+	strcpy(f2_cp, f2->nome);
+	char **v2 = tokenize_stopwords(f2_cp, &nv2, NAO_ALFA, stopwords);
+
+	int correspondencias = match(v1, v2, nv1, nv2);
+	
+	return (double) correspondencias / (double) fmax(nv1, nv2);
+}
+
+double filme_calcula_peso_ano(FILME *f1, FILME *f2) {
+	return 1 / (fabs((int) f1->ano - (int) f2->ano) / RELACAO_MAXIMA_ANO + 1); 
+}
+
+double filme_calcula_peso_generos(FILME *f1, FILME *f2) {
+	int correspondencias = match(f1->generos, f2->generos, f1->n_generos, f2->n_generos);
+	return (double) correspondencias / (double) fmax(f1->n_generos, f2->n_generos);
+}
+
+double filme_calcula_peso_sinopse(DICIONARIO *d, DICIONARIO *stopwords, FILME *f1, FILME *f2) {
+	/* Vetores binários (1 = contém, 0 = não contém) */
+	unsigned correspondencias1[dicionario_numero_palavras(d)];
+	unsigned correspondencias2[dicionario_numero_palavras(d)];
+
+	int nv1 = 0, nv2 = 0;
+
+	char f1_cp[strlen(f1->sinopse)];
+	strcpy(f1_cp, f1->sinopse);
+	char **v1 = tokenize_stopwords(f1_cp, &nv1, NAO_ALFA, stopwords);
+
+	char v1_e[nv1][51];
+	for(int i = 0; i < nv1; i++)
+		strcpy(v1_e[i], v1[i]);
+
+	char f2_cp[strlen(f2->sinopse)];
+	strcpy(f2_cp, f2->sinopse);
+	char **v2 = tokenize_stopwords(f2_cp, &nv2, NAO_ALFA, stopwords);
+
+	char v2_e[nv2][51];
+	for(int i = 0; i < nv2; i++)
+		strcpy(v2_e[i], v2[i]);
+
+	qsort(v1_e, nv1, sizeof(char) * 51, comparar_nome);
+	qsort(v2_e, nv2, sizeof(char) * 51, comparar_nome);
+
+	for(int i = 0; i < dicionario_numero_palavras(d); i++) {
+		correspondencias1[i] = buscar(dicionario_buscar_pos(d, i), nv1, sizeof(char) * 51, v1_e);
+		correspondencias2[i] = buscar(dicionario_buscar_pos(d, i), nv2, sizeof(char) * 51, v2_e);
+	}
+
+	for(int i = 0; i < nv1; i++) free(v1[i]);
+	for(int i = 0; i < nv2; i++) free(v2[i]);
+
+	free(v1);
+	free(v2);
+
+	return cosseno(correspondencias1, correspondencias2, dicionario_numero_palavras(d));
+}
+
+char *filme_nome(FILME *filme){
+	return filme->nome;
+}
+
+void filme_liberar(FILME *filme) {
+	free(filme->nome);
+	
+	for(int i = 0; i < filme->n_generos; i++)
+		free(filme->generos[i]);
+	
+	free(filme->generos);
+	free(filme->sinopse);
+	free(filme);
+}
+
+void filme_imprimir(FILME *filme) {
+	printf(ANSI_COR_PRETO_BRILHANTE "nome: %s\n", filme->nome);
+	printf("ano: %d\n", filme->ano);
+	printf("generos: ");
+
+	for(int i = 0; i < filme->n_generos; i++) {
+		printf("%s", filme->generos[i]);
+		if(i + 1 < filme->n_generos)
+			printf(", ");
+	}
+
+	printf("\n");
+	printf("sinopse: %s", filme->sinopse, ANSI_COR_RESET);
+}
+
+void filme_imprimir_lista(FILME *filme) {
+	printf(ANSI_COR_PRETO_BRILHANTE "%s (%d) | ", filme->nome, filme->ano);
+	for(int i = 0; i < filme->n_generos; i++) {
+		printf("%s", filme->generos[i]);
+		if(i + 1 < filme->n_generos)
+			printf(", ");
+	}
+
+	printf(ANSI_COR_RESET "");
+}
---
+++ recommender-master/grafo.h
@@ -0,0 +1,24 @@
+#ifndef GRAFO_H
+#define GRAFO_H
+
+#define PONDERACAO_ANO 1
+#define PONDERACAO_SINOPSE 2
+#define PONDERACAO_NOME 3
+#define PONDERACAO_GENEROS 4
+#define PESO_MINIMO 0.4
+
+#define RECOMENDACAO_SINOPSE 0
+#define RECOMENDACAO_GERAL 1
+#define NAO_RELACIONADOS 2
+#define MESMO_GENERO 3
+
+typedef struct grafo GRAFO;
+
+GRAFO *grafo_criar(FILE *arquivo);
+void grafo_liberar(GRAFO *grafo);
+void grafo_imprimir_filmes(GRAFO *grafo);
+void grafo_buscar(GRAFO *grafo, char *nome);
+void grafo_recomendar(GRAFO *grafo, char *nome, int peso_base);
+void grafo_imprimir_filmes_genero	(GRAFO* grafo, char* genero);
+
+#endif
